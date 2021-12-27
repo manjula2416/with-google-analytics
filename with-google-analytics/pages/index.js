8087db5dc186bedec23385f56c216538fb86da79
manjula2416 <hmanjulah1997@gmail.com>
2021-12-27 15:16:02 +0530

    initial commit

---
+++ with-google-analytics/pages/index.js
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import * as ga from '../lib/ga'
+
+export default function Home() {
+  const [query, setQuery] = useState("");
+
+  const search = () => {
+    ga.event({
+      action: "search",
+      params : {
+        search_term: query
+      }
+    })
+  }
+
+  return (
+    <div>
+        <div>
+          <input type="text" onChange={(event) => setQuery(event.target.value)}></input>
+        </div>
+        <div>
+        	<button onClick={() => search()}>Search</button>
+        </div>
+    </div>
+  )
+}
